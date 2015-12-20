6262d0bc22910a863b6f7558e5e9cb74879db549
tslator <theslators@gmail.com>
2015-12-20 09:00:49 -0800

    Changed propellerbot to arlobot, modified status and relay msgs, changed file naming convention to lowercase with underscores

--- src/arlobot/arlobot_bringup/scripts/arlobotpropiftest.py
+++ src/arlobot/arlobot_bringup/scripts/arlobotpropiftest.py
@@ -1,16 +1,33 @@
+from __future__ import print_function
 __author__ = 'Tim'
 
 import time
-from SerialDataGateway import SerialDataGateway
-from SerialMessage import SerialMessage, SerialMessageError
+from serial_data_gateway import SerialDataGateway
+from serial_msg import SerialMessage, SerialMessageError
 
-import sys
-if sys.version[0]=="2": input=raw_input
+from sys import platform as _platform
+
+
+if _platform == "linux" or _platform == "linux2":
+    PORT = "/dev/ttyUSB0"
+elif _platform == "darwin":
+    # MAC OS X
+    pass
+elif _platform == "win32":
+    PORT = "COM7"
+
+try:
+    input = raw_input
+except:
+    pass
 
 done = False
 
 op_state = ""
 odometry = ""
+analog_ir_sensor = ""
+digital_ir_sensor = ""
+ultrasonic_sensor = ""
 
 #c:d:0.403,0.00676
 #c:p:0,0,0,0,0,0.00,0.00,0.00
@@ -20,6 +37,8 @@ operational_state = SerialMessage(SerialMessage.CONFIG_OP_STATE_COMMAND, [0,0,0,
 
 dataReceiver = None
 configured = False
+safety_enable = True
+enable_odometry_print = False
 
 def ShowOpState():
     print(op_state)
@@ -27,10 +46,70 @@ def ShowOpState():
 def ShowOdometry():
     print(odometry)
 
+def ShowAnalogIRSensors():
+    print(analog_ir_sensor)
+
+def ShowDigitalIRSensors():
+    print(digital_ir_sensor)
+
+def ShowUltrasonicSensors():
+    print(ultrasonic_sensor)
+
+def ToggleSafety():
+    global safety_enable
+    ignore_proximity = False
+    ignore_cliff_sensors = False
+    ignore_dist_sensors = False
+    ignore_floor_sensors = False
+    ac_power = False
+
+    parsed_odom = odometry.split(':')[2].split(',')
+    # clear ignore_proximity in op_state message to enable safety
+    ignore_proximity = False
+    if (safety_enable):
+        safety_enable = False
+        ignore_proximity = True
+        ignore_proximity = True
+        ignore_cliff_sensors = True
+        ignore_dist_sensors = True
+        ignore_floor_sensors = True
+        ac_power = True
+        print("Safety is disabled")
+    else:
+        # set ignore_proximity in op_state message to disable safety
+        ignore_proximity = False
+        ignore_cliff_sensors = False
+        ignore_dist_sensors = False
+        ignore_floor_sensors = False
+        ac_power = False
+        safety_enable = True
+        print("Safety is enabled")
+
+    op_state = SerialMessage(SerialMessage.CONFIG_OP_STATE_COMMAND,
+                             [int(ignore_proximity),
+                              int(ignore_cliff_sensors),
+                              int(ignore_dist_sensors),
+                              int(ignore_floor_sensors),
+                              int(ac_power),
+                              parsed_odom[0], parsed_odom[1], parsed_odom[2]])
+    print(op_state.msg)
+    dataReceiver.Write(op_state.msg)
+
+def DisableSafety():
+    global safety_enable
+    # Set ignore_proximity in op_state message
+    parsed_odom = odometry.split(':')[2].split(',')
+    op_state = SerialMessage(SerialMessage.CONFIG_OP_STATE_COMMAND, [1, 0, 0, 0, 0, parsed_odom[0], parsed_odom[1], parsed_odom[2]])
+    #print(op_state.msg)
+    dataReceiver.Write(op_state.msg)
+    safety_enable = False
+    PrintSafetyState()
+
 def DoMove(vel):
     lin_vel, ang_vel = vel.split(',')
     move = SerialMessage(SerialMessage.ACTION_MOVE_COMMAND, [float(lin_vel), float(ang_vel)])
-    dataReceiver.Write(move.data)
+    print(move.msg)
+    dataReceiver.Write(move.msg)
 
 def Test(num):
     if num == 1:
@@ -60,32 +139,44 @@ def Test(num):
         DoMove("0.0,-1.0")
         time.sleep(1.0)
 
-def _handle_raw_message(line):
-    print(line)
+last_time = int(round(time.time() * 1000))
+delta = 0;
 
 def _handle_received_line(line):
     global odometry
     global op_state
+    global analog_ir_sensor
+    global digital_ir_sensor
+    global ultrasonic_sensor
     global dataReceiver
     global configured
+    global last_time
+    global delta
+
+    delta = int(round(time.time() * 1000)) - last_time
+    last_time = int(round(time.time() * 1000))
+
+    #print(delta, ": ", line)
 
     try:
         msg = SerialMessage(data=line)
-    except SerialMessageError:
-        #print(line)
+    except SerialMessageError as sme:
+        print(sme)
+        print(line)
         return
 
     if msg.msg_class == SerialMessage.STATUS_CLASS:
+        #print("Class: ", msg.msg_class, " Type: ", msg.msg_type, " Msg: ", msg.msg)
         if msg.msg_type == SerialMessage.STATUS_OP_STATE_MESSAGE:
             op_state = line
 
             if not msg.drive_geometry_received:
                 print("Sending drive geometry")
-                dataReceiver.Write(drive_geometry.data)
+                dataReceiver.Write(drive_geometry.msg)
 
             if not msg.op_state_received:
                 print("Sending operational state")
-                dataReceiver.Write(operational_state.data)
+                dataReceiver.Write(operational_state.msg)
 
             if not configured:
                 if msg.drive_geometry_received:
@@ -100,7 +191,17 @@ def _handle_received_line(line):
 
         elif msg.msg_type == SerialMessage.STATUS_ODOMETRY_MESSAGE:
             odometry = line
-            #print(odometry)
+            if enable_odometry_print:
+                print(delta, ": ", odometry)
+        elif msg.msg_type == SerialMessage.STATUS_ANALOG_IR_SENSOR_MESSAGE:
+            analog_ir_sensor = line
+            #print(delta, ": ", analog_ir_sensor)
+        elif msg.msg_type == SerialMessage.STATUS_DIGITAL_IR_SENSOR_MESSAGE:
+            digital_ir_sensor = line
+            #print(delta, ": ", digital_ir_sensor)
+        elif msg.msg_type == SerialMessage.STATUS_ULTRASONIC_SENSOR_MESSAGE:
+            ultrasonic_sensor = line
+            #print(delta, ": ", ultrasonic_sensor)
         else:
             print("Unknown type: ", line)
 
@@ -116,9 +217,14 @@ def main():
     global done
     global dataReceiver
     global configured
+    global odometry
+    global op_state
+    global analog_ir_sensor
+    global digital_ir_sensor
+    global ultrasonic_sensor
+    global enable_odometry_print
 
-    #dataReceiver = SerialDataGateway("COM3", 115200, _handle_received_line)
-    dataReceiver = SerialDataGateway("/dev/ttyUSB2", 115200, _handle_received_line)
+    dataReceiver = SerialDataGateway(PORT, 115200, _handle_received_line)
     dataReceiver.Start()
 
     DoMove("0.0,0.0")
@@ -132,6 +238,10 @@ def main():
         print("The following commands are supported:")
         print("    Show Op State - s")
         print("    Show Odometry - o")
+        print("    Show Analog IR - i")
+        print("    Show Digital IR - g")
+        print("    Show Ultrasonic - u")
+        print("    Toggle Safety - y")
         print("    Move - m:<linear velocity>,<angular velocity>")
         print("    Test - t:<number>")
         print("    Exit - x")
@@ -140,12 +250,25 @@ def main():
             ShowOpState()
         elif result == 'o':
             ShowOdometry()
+        elif result == 'i':
+            ShowAnalogIRSensors()
+        elif result == 'g':
+            ShowDigitalIRSensors()
+        elif result == 'u':
+            ShowUltrasonicSensors()
+        elif result == 'y':
+            ToggleSafety()
+            print(op_state)
         elif 'm' in result:
             cmd, vel = result.split(':')
+            enable_odometry_print = True
             DoMove(vel)
+            enable_odometry_print = False
         elif 't' in result:
             cmd, num = result.split(':')
+            enable_odometry_print = True
             Test(int(num))
+            enable_odometry_print = False
         elif result == 'x':
             DoMove("0.0,0.0")
             done = True
